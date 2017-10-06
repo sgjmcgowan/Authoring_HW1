44be0536c4161bfe8d55c0c842c74baa5980b338
sgjmcgowan <31935074+sgjmcgowan@users.noreply.github.com>
2017-10-06 18:58:29 -0400

    done

--- Authoring_HW1-master/script/contentObject.js
+++ Authoring_HW1-master/script/contentObject.js
@@ -6,12 +6,20 @@ var dynamicContent = {
 
 			images : [
 				"spring_1.jpg",
-				"spring_2.jpg"
+				"spring_2.jpg",
+				"spring_3.jpg",
+				"spring_4.jpg",
+				"spring_5.jpg",
+				"spring_6.jpg"
 			],
 
 			imageDescription : [
 				'This is some pretty butterflies',
-				'This is some kind of mutant chicken'
+				'This is some kind of mutant chicken',
+				'Here are some flowers',
+				'Butterflies like pink, I guess.',
+				'The Sun is shining through some trees.',
+				'These birds are considered uncool by their friends.'
 			]
 		},
 
@@ -22,7 +30,20 @@ var dynamicContent = {
 
 			images : [
 				"summer_1.jpg",
-				"summer_2.jpg"
+				"summer_2.jpg",
+				"summer_3.jpg",
+				"summer_4.jpg",
+				"summer_5.jpg",
+				"summer_6.jpg"
+			],
+
+			imageDescription : [
+				'Chairs on a beach',
+				'Doggo in a hammock',
+				'Chiplord, Great King of Doritoland',
+				'How to Get a Weird Sunburn 101.',
+				'An island, probably.',
+				'A starfish, long dead.'
 			]
 		},
 
@@ -33,7 +54,20 @@ var dynamicContent = {
 
 			images : [
 				"autumn_1.jpg",
-				"autumn_2.jpg"
+				"autumn_2.jpg",
+				"autumn_3.jpg",
+				"autumn_4.jpg",
+				"autumn_5.jpg",
+				"autumn_6.jpg"
+			],
+
+			imageDescription : [
+				'A beautiful Autumn stroll',
+				'Pumpkins are normal and sunflowers are weird.',
+				'That leaf is stuck to gum.',
+				'More trees.',
+				'Leaves and trees are kind of an Autumn thing.',
+				'Bike down here, forget about eveything.'
 			]
 		},
 
@@ -44,7 +78,20 @@ var dynamicContent = {
 
 			images : [
 				"winter_1.jpg",
-				"winter_2.jpg"
+				"winter_2.jpg",
+				"winter_3.jpg",
+				"winter_4.jpg",
+				"winter_5.jpg",
+				"winter_6.jpg"
+			],
+
+			imageDescription : [
+				'The dawn in wintertime',
+				'The Rankin-Bass animation was never good.',
+				'Cold puppers.',
+				'An Umbrella is more of a Spring thing, really.',
+				'Placid water that is deadly cold.',
+				'Aurora was the meanest girl in my high school.'
 			]
 		}
 	};
